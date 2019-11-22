36d435fc86e6d793cb723e1096084cff52649500
Douglas Ciarfella <losdougie@comcast.net>
2019-11-21 20:14:03 -0500

    Updated todo model to use choices

--- todo/kanban/views.py
+++ todo/kanban/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from .models import ToDo
 
 # Create your views here.
 def index(request):
+    all_todos = ToDo.objects.exclude(status=4).order_by('-id')
+    data = []
+    for todo in all_todos:
+        data.append(todo.status)
+
+
     context = {
         "title": "Home",
-        "data": "Hello World"
+        "data": data,
+        "todos": all_todos
     }
     return render(request, 'kanban/home.html', context)
